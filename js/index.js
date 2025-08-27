4702e448d3b0d1f77bb12f0a193d9ef2ee435257
Mamun <mamunreza2025@gmail.com>
2025-08-27 23:40:45 +0600

    added a js ar call button funtionality

---
+++ js/index.js
@@ -0,0 +1,61 @@
+
+                 
+// call button  start
+const callButtons=document.getElementsByClassName("call-button");
+                 
+            // coyent button add
+            const coyentBtn=document.getElementById("coyent-btn");
+                 coyentBtn.innerText=100;            
+            
+     
+for (const callButton of callButtons) {
+        callButton.addEventListener('click',function(){
+            // console.log("button click paise..")
+            
+            const title=callButton.parentNode.parentNode.children[1].children[0].innerText;
+            const subTitle=callButton.parentNode.parentNode.children[1].children[1].innerText;
+            const callNumber=callButton.parentNode.parentNode.children[1].children[2].innerText;
+            
+            // coyent btn 20 kore kombe
+            const lessCoyent=20;
+
+            const abaiAbleCoyent=Number(coyentBtn.innerText) -lessCoyent;
+                       // current coyent cheek
+            if (abaiAbleCoyent<0) {
+                alert("unsuficent coyent....");
+                return;
+                
+            }
+
+                  coyentBtn.innerText=abaiAbleCoyent;
+            
+                  
+          alert(  subTitle +" "+ callNumber);
+
+            const date=new Date().toLocaleTimeString();
+            
+            const sideBar=document.getElementById("side-bar");
+           const div= document.createElement("div")
+           div.innerHTML=`
+
+                    <div class="side-card-container">
+           <div class="flex justify-between items-center w-[270px] h-[80px] mx-auto bg-[#FAFAFA] px-2 rounded-md shadow mb-5">
+             <div class="">
+              <h2 class="text-[16px] font-normal">${title}</h2>
+              <p>${callNumber}</p>
+             </div> 
+             <div>
+              <p>${date}</p>
+             </div>    
+                          
+       </div>
+
+
+    </div>
+           `
+
+           sideBar.append(div);
+        })
+    };
+
+    // call button end
